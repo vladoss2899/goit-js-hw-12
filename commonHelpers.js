83cd27dc98e92008a63e82a5c3aa7fe1dd05d1d4
vladoss2899 <136236185+vladoss2899@users.noreply.github.com>
2024-01-31 19:47:39 +0000

    Deploying to gh-pages from @ vladoss2899/goit-js-hw-12@6de710ab4a9f509a9dbbc31aee3754211fac90ac 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,26 +1,26 @@
-import{S as w,i as u,a as P}from"./assets/vendor-990f3500.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))a(e);new MutationObserver(e=>{for(const s of e)if(s.type==="childList")for(const c of s.addedNodes)c.tagName==="LINK"&&c.rel==="modulepreload"&&a(c)}).observe(document,{childList:!0,subtree:!0});function i(e){const s={};return e.integrity&&(s.integrity=e.integrity),e.referrerpolicy&&(s.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?s.credentials="include":e.crossorigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function a(e){if(e.ep)return;e.ep=!0;const s=i(e);fetch(e.href,s)}})();document.addEventListener("DOMContentLoaded",()=>{const p=document.querySelector(".search-form"),o=document.querySelector(".gallery"),i=document.querySelector(".loader"),a=document.querySelector('[data-action="load-more"]'),e=document.querySelector(".finish-loader"),s="https://pixabay.com/api",c="41900218-778e908913d1efd90b8f97d56",t={query:"",page:1,maxPage:0,pageSize:40},g=new w(".gallery a",{captionsData:"alt",captionDelay:250}),d={position:"topRight",backgroundColor:"red",icon:"none"};p.addEventListener("submit",f),a.addEventListener("click",h);async function f(r){r.preventDefault(),i.style.display="block",a.classList.add("is-hidden"),e.classList.add("is-hidden"),o.innerHTML="";const n=r.currentTarget;if(t.query=n.elements.query.value.trim(),t.page=1,!t.query){i.style.display="none",u.error({...d,message:"Enter a query value, please"});return}try{const l=await m(t.query);l.data.hits.length===0?u.error({...d,message:"Sorry, there are no images matching your search query. Please try again!"}):(y(l.data.hits),t.maxPage=Math.ceil(l.data.totalHits/t.pageSize)),t.page===t.maxPage?(e.classList.remove("is-hidden"),a.classList.add("is-hidden")):a.classList.remove("is-hidden"),n.reset()}catch{u.error({...d,message:"Oops, server connection error!"})}finally{i.style.display="none"}}async function m(r){const n=new URLSearchParams({key:c,q:r,image_type:"photo",orientation:"horizontal",safesearch:"true",per_page:t.pageSize,page:t.page});return P.get(`${s}/?${n}`)}async function h(){t.page+=1,i.style.display="block",a.classList.add("is-hidden");try{const r=await m(t.query);y(r.data.hits),t.page===t.maxPage?(a.classList.add("is-hidden"),e.classList.remove("is-hidden")):(a.classList.remove("is-hidden"),window.scrollBy(0,window.innerHeight))}catch{u.error({...d,message:"Oops, server connection error!"})}finally{i.style.display="none"}}function y(r){const n=r.map(({largeImageURL:l,webformatURL:v,tags:L,likes:q,views:S,comments:x,downloads:b})=>`
-            <li class="gallery-card">
-                <a class="gallery-link" href="${l}">
-                    <img class="gallery-image" src="${v}" alt="${L}"/>
-                </a>
-                <div class="titles-box">
-                    <div class="title-element">
-                        <p class="title-text">Likes:</p>
-                        <p class="title-value">${q}</p>
-                    </div>
-                    <div class="title-element">
-                        <p class="title-text">Views:</p>
-                        <p class="title-value">${S}</p>
-                    </div>
-                    <div class="title-element">
-                        <p class="title-text">Comments:</p>
-                        <p class="title-value">${x}</p>
-                    </div>
-                    <div class="title-element">
-                        <p class="title-text">Downloads:</p>
-                        <p class="title-value">${b}</p>
-                    </div>
+import{S as E,i as u,a as $}from"./assets/vendor-990f3500.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))r(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const c of t.addedNodes)c.tagName==="LINK"&&c.rel==="modulepreload"&&r(c)}).observe(document,{childList:!0,subtree:!0});function i(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerpolicy&&(t.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?t.credentials="include":e.crossorigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function r(e){if(e.ep)return;e.ep=!0;const t=i(e);fetch(e.href,t)}})();document.addEventListener("DOMContentLoaded",()=>{const p=document.querySelector(".search-form"),o=document.querySelector(".gallery"),i=document.querySelector(".loader"),r=document.querySelector('[data-action="load-more"]'),e=document.querySelector(".finish-loader"),t="https://pixabay.com/api",c="41900218-778e908913d1efd90b8f97d56",s={query:"",page:1,maxPage:0,pageSize:40},h=new E(".gallery a",{captionsData:"alt",captionDelay:250}),d={position:"topRight",backgroundColor:"red",icon:"none"};p.addEventListener("submit",L),r.addEventListener("click",v);async function L(a){a.preventDefault(),i.style.display="block",r.classList.add("is-hidden"),e.classList.add("is-hidden"),o.innerHTML="";const n=a.currentTarget;if(s.query=n.elements.query.value.trim(),s.page=1,!s.query){i.style.display="none",u.error({...d,message:"Enter a query value, please"});return}try{const l=await m(s.query);l.data.hits.length===0?u.error({...d,message:"Sorry, there are no images matching your search query. Please try again!"}):(y(l.data.hits),s.maxPage=Math.ceil(l.data.totalHits/s.pageSize)),g(),n.reset()}catch{f()}}async function m(a){const n=new URLSearchParams({key:c,q:a,image_type:"photo",orientation:"horizontal",safesearch:"true",per_page:s.pageSize,page:s.page});return $.get(`${t}/?${n}`)}async function v(){s.page+=1,i.style.display="block",r.classList.add("is-hidden");try{const a=await m(s.query);y(a.data.hits),g()}catch{f()}}function y(a){const n=a.map(l=>{const{largeImageURL:q,webformatURL:b,tags:S,likes:x,views:w,comments:k,downloads:P}=l;return`
+        <li class="gallery-card">
+            <a class="gallery-link" href="${q}">
+                <img class="gallery-image" src="${b}" alt="${S}"/>
+            </a>
+            <div class="titles-box">
+                <div class="title-element">
+                    <p class="title-text">Likes:</p>
+                    <p class="title-value">${x}</p>
                 </div>
-            </li>
-        `).join("");o.insertAdjacentHTML("beforeend",n),g.refresh()}});
+                <div class="title-element">
+                    <p class="title-text">Views:</p>
+                    <p class="title-value">${w}</p>
+                </div>
+                <div class="title-element">
+                    <p class="title-text">Comments:</p>
+                    <p class="title-value">${k}</p>
+                </div>
+                <div class="title-element">
+                    <p class="title-text">Downloads:</p>
+                    <p class="title-value">${P}</p>
+                </div>
+            </div>
+        </li>
+      `}).join("");o.insertAdjacentHTML("beforeend",n),h.refresh()}function g(){s.page===s.maxPage?(e.classList.remove("is-hidden"),r.classList.add("is-hidden")):r.classList.remove("is-hidden"),i.style.display="none"}function f(){u.error({...d,message:"Oops, server connection error!"}),i.style.display="none"}});
 //# sourceMappingURL=commonHelpers.js.map
